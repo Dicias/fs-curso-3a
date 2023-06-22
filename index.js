26bd7d9c7fe5f2ad424c7f5a24a36e63f4fa324a
dicias <dmacias26@alumnos.uaq.mx>
2023-06-21 20:39:55 -0600

    ejercicios 3.1/3.6 completos

--- index.js
+++ index.js
@@ -1,85 +1,111 @@
+// Desc: Backend for phonebook app
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 
-let notes = [
+let persons = [
     {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
     },
     {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
     },
     {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122"
     }
-  ]
+]
 
-
-
-app.get('/', (request, response)=>{
-    response.send('<h1>Hello World from </h1>')
+app.get('/',(request, response)=>{
+    response.send('<h1>Hello World from persons</h1>')
 })
 
-
-app.get ('/api/notes',(request, response) =>{
-    response.json(notes)
+app.get('/api/persons',(request, response)=>{
+    response.json(persons)
 })
 
-app.get('/api/notes/:id', (request, response)=>{
-    const id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => note.id === id)
-    console.log(note);
+app.get('/info', (request,response)=>{
+    const date = new Date
+    response.send(`<p>Phonebook has info for ${persons.length}</p>
+    <p> ${date}</p> `)
+})
 
-    if(note){
-      response.json(note)
-    }else{
-      response.status(404).end()
-    }
-  })
+app.get('/api/persons/:id', (request, response)=>{
+const id = Number(request.params.id)
+console.log(typeof id)
+const person = persons.find(person => person.id === id)
+console.log(person, 'desde person')
+if(person){
+    response.json(person)
+}else{
+    response.status(404).end()
+}
+})
 
-  app.delete('/api/notes/:id', (request, response)=>{
+app.delete('/api/persons/:id', (request, response)=>{
     const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+    const newPersons = persons.filter(person => person.id !== id)
+    persons = newPersons
     response.status(204).end()
-  })
+})
 
-  const generateId = () =>{
-    const maxId = notes.length > 0
-    ?Math.max(...notes.map(n => n.id)):0
-    return maxId + 1
-  }
+//// test ////
+app.get('/api/persons/test/:id', (request, response)=>{
+    const theId = generateId()
+    response.json(theId)
+})
 
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
+
+const generateId = () =>{
+    const genId = Math.random() * (500 - 100) + 100
+    const newId = Math.floor(genId)
+    return newId
+}
+
+const sameName = (name) =>{
+    const same = persons.find(person => person.name === name)
+    return same
     
-    if(!body.content){
-      return response.status(400).json({
-        error: 'content missing'
-      })
-    }
-    const note= {
-      content: body.content,
-      important: body.important || false ,
-      date: new Date(),
-      id: generateId()
-    }
+}
 
-    notes = notes.concat(note)
-    response.json(note)
-  })
+app.post('/api/persons', (request, response)=>{
+const body = request.body
+console.log(sameName(body.name));
 
+if(!body.name){
+    return response.status(400).json({
+        error: 'name missing'                
+    })
+}else if(!body.number){
+    return response.status(400).json({
+        error: 'number missing'
+    })
+}else if(sameName(body.name)){
+    return response.status(400).json({
+        error: 'name must be unique'
+    })
+}
+
+const newPerson ={
+    id:generateId(),
+    name: body.name,
+    number: body.number
+}
+persons = persons.concat(newPerson)
+response.json(newPerson)
+})
 
 const PORT = 3001
 app.listen(PORT)
-console.log(`server running on port ${PORT}`)
+console.log(`Server running on port ${PORT}`)
